78aea2c8ad7cabaaff52a0a95133086fdaa4503c
PedroP0202 <pedrokeciosousadapiedade@hotmail.com>
2026-07-26 22:00:37 +0100

    fix1

--- config/settings.py
+++ config/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'chave-local-desenvolvimento')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
-CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',')
+CSRF_TRUSTED_ORIGINS = [o for o in os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',') if o]
 
 
 INSTALLED_APPS = [
